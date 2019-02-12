12b25713452f891c966bbb64c287a04e46d33526
sabirzha <sabirzha@gmail.com>
2019-02-12 15:55:02 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,125 @@
+export const productsApi = [
+    {
+        id: 1,
+        price: 459.99,
+        title: "Guitar",
+        img: "img/product-1.png",
+        company: "zzz",
+        info: "vbxvcb bx cvxb xcbcbcxb xb cx bxc bxb xcb c vcb",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        price: 3300,
+        title: "Baby Piano",
+        img: "img/product-2.png",
+        company: "zzz",
+        info: "sf  dfg sdg gbg sgddgsdg gsa aa gsgasf gas g cvxc x",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        price: 9999,
+        title: "Grand Piano",
+        img: "img/product-3.png",
+        company: "zzz",
+        info: "f as sf  xcvxcvzv asf sdf",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        price: 799,
+        title: "Couch",
+        img: "img/product-4.png",
+        company: "xxx",
+        info: "adas dasdas dssa das da asd asdasd ad ada",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        price: 1300,
+        title: "Dining Table",
+        img: "img/product-5.png",
+        company: "xxx",
+        info: "dfgshshd sgds gfs d fdg sdf gsdfgsdfgssdgsdf sdgsdg ",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        price: 100,
+        title: "Chair",
+        img: "img/product-6.png",
+        company: "xxx",
+        info: "gds gd ds gsfd gsdgdgsd gsdgdsgdsgsdfgsdgsdd ds gfd",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        price: 355,
+        title: "Leather Jacket",
+        img: "img/product-7.png",
+        company: "yyy",
+        info: "sadf sa3wwe rwer weafwaer3 rfasdasd fsda",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        price: 150,
+        title: "Classic Jeans",
+        img: "img/product-8.png",
+        company: "yyy",
+        info: "2343432 23 24 23 4543 23 2422 2",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 9,
+        price: 50,
+        title: "Shorts",
+        img: "img/product-9.png",
+        company: "yyy",
+        info: "2342 42 242323 23 423 2",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 10,
+        price: 379,
+        title: "Shoes",
+        img: "img/product-10.png",
+        company: "yyy",
+        info: "df asf sfs saf asdfasd fasgf fwe rfvcccxvxc v werfwe rsdfs sadfasfsafcxv  vs",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+
+export const detailProduct = {
+    
+        id: 1,
+        price: 459.99,
+        title: "Guitar",
+        img: "img/product-1.png",
+        company: "zzz",
+        info: "vbxvcb bx cvxb xcbcbcxb xb cx bxc bxb xcb c vcb asdjafhfdsifhfdsfsfsdddfsdf fsafassdfsagsgsagsdgsdahasg asgassgsag",
+        inCart: false,
+        count: 0,
+        total: 0
+}
